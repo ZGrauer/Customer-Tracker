28d562ba3c04f91b190fd09ac67e7556c4f76dd4
ZGrauer <zgrauer@wgu.edu>
2017-07-15 14:21:00 -0500

    fix:  Fix styling of title in header

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -8,16 +8,19 @@ import '../../public/stylesheets/font-awesome-4.7.0/css/font-awesome.min.css';
 
 @Component({
     selector: 'app-header',
-    template: `
-        <h1>Customer Tracker</h1>
-        <p-tabMenu [(model)]="items"></p-tabMenu>
-    `
+    templateUrl: 'header.component.html',
+    styles: [`
+            #title {
+                text-align: center;
+                font-size: 38px;
+                letter-spacing: 3px;
+            }
+        `]
 })
 export class HeaderComponent implements OnInit {
     items: MenuItem[];
-
     constructor(private authService: AuthService) {}
-
+    
     ngOnInit() {
         this.items = [
             {label: 'Customers', icon: 'fa-table' , routerLink: ['customers']},
